a2df397de876d802f0a7c6ceede581eb1f14fdde
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-13 22:01:13 +0500

    danmhvs dja d

---
+++ Unit-testing/Dtos/UserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Unit_testing.Dtos
+{
+    public class UserDto
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
