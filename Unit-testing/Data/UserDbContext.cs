477ecb4c550b70004eaa43ecf1aded16f02c7177
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-13 22:01:09 +0500

    hjavdkj,s cksHKCb

---
+++ Unit-testing/Data/UserDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Unit_testing.Entities;
+
+namespace Unit_testing.Data
+{
+    public class UserDbContext : DbContext
+    {
+        public UserDbContext(DbContextOptions<UserDbContext> options) 
+            : base(options)
+            { }
+        public DbSet<User> Users { get; set; }
+
+    }
+}
