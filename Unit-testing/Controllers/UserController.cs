769315d61dac2edfe4d2b9f64d65649c70ef23b7
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-13 22:01:00 +0500

    hbdjhkads

---
+++ Unit-testing/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Unit_testing.Data;
+using Unit_testing.Dtos;
+
+namespace Unit_testing.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserDbContext _context;
+
+        public UserController(UserDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async ValueTask<IActionResult> GetAllAsync()
+        {
+            var result = await _context.Users.ToListAsync();
+            return Ok(result);
+        }
+
+        
+    }
+}
